c8875bed60a8e1f6ff46969fb0ab66a127b09ae3
kkotam <gojug115@gmail.com>
2026-07-11 11:31:53 +0900

    쇼핑몰 프로젝트

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section className="app">
+        <h2>쇼핑몰 라우팅</h2>
+      </section>
+
+    </>
+  )
+}
+
+export default App
